358ead64df90d643e33e65fbb811f265df294fa2
Arpita <arpitajana2000@gmail.com>
2026-02-11 18:52:46 +0530

    chore: setup API client

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://127.0.0.1:8000/api/",
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (
+      err.response?.status === 401 &&
+      !err.config.url.includes("auth/login")
+    ) {
+      localStorage.clear();
+      window.location.href = "/";
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default api;
