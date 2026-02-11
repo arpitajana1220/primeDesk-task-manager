76f0dfa7f208750a306e4785627f4ce2ac6961bd
Arpita <arpitajana2000@gmail.com>
2026-02-11 12:26:26 +0530

    feat: update fields in task model

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -3,7 +3,8 @@ from .models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Task
         fields = "__all__"
-        read_only_fields = ["user"]
+        read_only_fields = ["user", "created_at"]
